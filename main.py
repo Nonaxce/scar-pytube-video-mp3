1ae8439682ec0bb0962b70b301bc2868756238b6
Nuel Nonaxce <agsisasleep@gmail.com>
2023-12-20 13:27:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,3 +43,80 @@ def isArrayPlural(url_arr):
         return "s"
     else:
         return ""
+
+# function that displays url
+def show_url_arr(url_arr):
+    # list number
+    i = 1
+    print(f"""   ----Download Queue---------""")
+    if (len(url_arr) != 0):
+        for x in url_arr:
+            if (i%2 == 0):
+                print(f"""     {i}.  \033[36m{x}\033[0m""")
+            else:
+                print(f"""     {i}.  \033[35m{x}\033[0m""")
+            i += 1
+    else:
+        print(f"""    Download queue empty :<""")
+    print(f"""   --------------------------""")
+    
+# collects user urls and then adds it the array
+def get_user_input():
+    collecting = False
+    # array for the urls
+    video_url_arr = []
+    # loops/keeps on asking for urls until the user says quit
+    while collecting == False:
+        # user input to string
+        video_url = str(input("Enter URL: "))
+        # checks if user wants to stop adding urls to queue
+        if (video_url == "1"):
+            show_url_arr(video_url_arr)
+            return video_url_arr
+            collecting = True
+        else:
+            # remove the previous url entered
+            if (video_url == "2"):
+                # checks kung wala na unod ang queue
+                if (len(video_url_arr) == 0):
+                    print("download queue is already empty ;-;")
+                else: 
+                    video_url_arr.pop()
+                    show_url_arr(video_url_arr)
+            # removes all urls in the list
+            elif (video_url == "3"):
+                video_url_arr.clear()
+                show_url_arr(video_url_arr)
+            else:
+                try:
+                    regex_url = '^https:\/\/www\.youtube\.com\/watch\?v='
+                    url_regex_match = re.match(regex_url, video_url)
+                    if(url_regex_match):
+                        # adds the url to the list 
+                        video_url_arr.append(video_url)
+                        show_url_arr(video_url_arr)
+                    else:
+                       print(f"'\033[31m{video_url}\033[0m' is not a url")
+
+                # if error does happen
+                except KeyError:
+                    print("Sorry something went wrong")
+
+
+def main():
+    print(f"""
+    +==============YT TO MP3===============+
+    |           Built with Pytube          |
+    +______________________________________+
+    | Enter 0 to quit                      |
+    | Enter 1 to stop                      |
+    | Enter 2 to remove previous           |
+    | Enter 3 to remove all                |
+    +--------------------------------------+
+    """)
+    video_arr = get_user_input()
+    downloadAudio(video_arr)
+
+
+# runs the program
+main()
